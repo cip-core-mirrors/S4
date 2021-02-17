a6a7e1da27cc26026e36168fa80926abda4071f5
Vladimir Rybalko (CZ) <Vladimir.Rybalko@absa.africa>
2021-02-17 10:50:49 +0100

    Review takeaway

--- src/S3ProxyPOC/Misc/HttpRequestExtensions.cs
+++ src/S3ProxyPOC/Misc/HttpRequestExtensions.cs
@@ -5,8 +5,8 @@ namespace ABSA.RD.S3Proxy.Misc
 {
     public static class HttpRequestExtensions
     {
-        public static string GetValue(this IQueryCollection query, string key) => query.TryGetValue(key, out var values) ? values.First() : default;
+        public static string GetValue(this IQueryCollection query, string key) => query.TryGetValue(key, out var values) ? values.Single() : default;
 
-        public static string GetValue(this IHeaderDictionary headers, string key) => headers.TryGetValue(key, out var values) ? values.First() : default;
+        public static string GetValue(this IHeaderDictionary headers, string key) => headers.TryGetValue(key, out var values) ? values.Single() : default;
     }
 }
