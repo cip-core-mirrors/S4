d3559c8e456e0a1ce7355521a638f80732016e16
Vladimir Rybalko (CZ) <Vladimir.Rybalko@absa.africa>
2021-02-22 11:35:12 +0100

    Added NLog infrastructure

---
+++ src/S3ProxyHost/Misc/LoggingBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Time;
+using System;
+using System.IO;
+
+namespace ABSA.RD.S4.S3ProxyHost.Misc
+{
+    public static class LoggingBuilderExtensions
+    {
+        public static ILoggingBuilder InitializeNLog(this ILoggingBuilder builder)
+        {
+            TimeSource.Current = new AccurateLocalTimeSource();
+
+            // Nlog.Config is using following variables we have to define.
+            GlobalDiagnosticsContext.Set("LogDir", Path.Combine(Path.GetTempPath(), "ABSA_OSS_S4_logs"));
+            GlobalDiagnosticsContext.Set("StartTime", DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss"));
+
+            // delete old logs
+            if (LogManager.Configuration.Variables.TryGetValue("deleteFilesAfterDays", out var deleteInterval))
+            {
+                if (Directory.Exists(GlobalDiagnosticsContext.Get("LogDir")))
+                {
+                    var files = Directory.EnumerateFiles(GlobalDiagnosticsContext.Get("LogDir"), "*." + LogManager.Configuration.Variables["logFileSuffix"].Text);
+                    foreach (var f in files)
+                    {
+                        if (new FileInfo(f).CreationTimeUtc + TimeSpan.FromDays(int.Parse(deleteInterval.Text)) < DateTime.UtcNow)
+                            File.Delete(f);
+                    }
+                }
+            }
+
+            return builder;
+        }
+    }
+}
--- src/S3ProxyHost/Startup.cs
+++ src/S3ProxyHost/Startup.cs
@@ -1,22 +1,53 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using Amazon.Runtime.CredentialManagement;
 using ABSA.RD.S4.S3Proxy.Misc;
 using ABSA.RD.S4.S3Proxy;
+using Microsoft.Extensions.Configuration;
+using NLog;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using ABSA.RD.S4.S3ProxyHost.Misc;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text;
+using System;
 
 namespace ABSA.RD.S4.S3ProxyHost
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IWebHostEnvironment env)
         {
+            LogManager.LoadConfiguration($"NLog.{env.EnvironmentName}.config");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {   
+            services.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddNLog();
+                logging.InitializeNLog();
+            });
+
+            services.AddCors();
+
             var config = new S3Config
             {
-                Region = "eu-west-1",
-                KmsKey = "arn:aws:kms:eu-west-1:***REMOVED***",
-                CredentialsProfile = new CredentialProfileStoreChain().TryGetAWSCredentials("saml", out var _) ? "saml" : null
+                Region = Configuration["S3:Region"],
+                KmsKey = Configuration["S3:KmsKey"],
+                CredentialsProfile = new CredentialProfileStoreChain().TryGetAWSCredentials(Configuration["S3:CredentialsProfile"], out var _) ? Configuration["S3:CredentialsProfile"] : null
             };
 
             services.AddS3Proxy(config);
@@ -32,9 +63,11 @@ namespace ABSA.RD.S4.S3ProxyHost
                 {
                     await worker.Run(context);
                 }
-                catch (Exception ex)
+                catch(Exception ex)
                 {
-
+                    context.Response.StatusCode = 500;
+                    await context.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(ex.Message));
+                    throw;
                 }
             });
         }
