ecd11f114ac94dc10dfa25a8a8b07fd21f5929c4
Miroslav Chomut (CZ) <Miroslav.Chomut@absa.africa>
2021-02-23 13:53:17 +0100

    s3bench output beautification

--- src/Tools/S3Bench/Program.cs
+++ src/Tools/S3Bench/Program.cs
@@ -1,7 +1,6 @@
 ﻿using ABSA.RD.S4.S3Bench.Settings;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ABSA.RD.S4.S3Bench
@@ -11,12 +10,9 @@ namespace ABSA.RD.S4.S3Bench
         static async Task Main()
         {
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
             var s3Handler = new S3Handler(config.GetSection("S3").Get<S3Settings>());
             var bench = new S3Bench(s3Handler, config.GetSection("Bench").Get<BenchSettings>());
-            var watch = Stopwatch.StartNew();
             await bench.RunTestAsync();
-            Console.WriteLine($"FINISHED in {watch.Elapsed} with {Process.GetCurrentProcess().PeakWorkingSet64}");
             Console.ReadKey();
         }
     }
